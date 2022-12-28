bfa3844379e459c017609ddca3ced2c69308c38e
pxsa <paghsis@gmail.com>
2022-12-28 09:13:36 +0330

    start implementing in c++

---
+++ implementation/c++/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    private:
+        int value;
+        int costToGoal;
+                
+
+};
+
+int main() {
+    cout << "Hello, World!!";
+    return 0;
+}
