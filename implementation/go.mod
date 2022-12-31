0a2fb5f6f2712456b5d90ebb48ba06d108fbf05a
pxsa <paghsis@gmail.com>
2022-12-31 17:33:31 +0330

    get new version of iGraph

--- implementation/go.mod
+++ implementation/go.mod
@@ -3,6 +3,6 @@ module piziStar
 go 1.19
 
 require (
-	github.com/pxsa/iGraph v0.0.0-20221226142830-21b95229ada5 // indirect
-	golang.org/x/exp v0.0.0-20221217163422-3c43f8badb15 // indirect
+	github.com/pxsa/iGraph v0.0.0-20221228060558-5d3251c46f29
+	golang.org/x/exp v0.0.0-20221227203929-1b447090c38c
 )
--- implementation/main.go
+++ implementation/main.go
@@ -1,64 +1,73 @@
 package main
 
 import (
-	"github.com/pxsa/iGraph"
+	// "github.com/pxsa/iGraph"
 )
 
 func main() {
 
-	goal := &myNode{
-		tag: "new",
-		Node: iGraph.Node{
-			Value: 2,
-		},
-		costToGoal: 0,
-		next: nil,
-	}
-	start := &myNode{
-		tag: "new",
+	// goal := &myNode{
+	// 	tag: "new",
+	// 	Node: iGraph.Node{
+	// 		Value: 2,
+	// 	},
+	// 	costToGoal: 0,
+	// 	next: nil,
+	// }
+	// start := &myNode{
+	// 	tag: "new",
 		
-	}
-
-	state1 := &myNode{
-		tag: "new",
-	}
-	state2 := &myNode{
-		tag: "new",
-	}
-	state3 := &myNode{
-		tag: "new",
-	}
-	states := []iGraph.CustomNode {state1, state2, state3}
-
-	edge1 := &iGraph.Edge {
-		Weight: 5,
-		Origin: start,
-		Destination: state1,
-		IsDirected: true,
-	}
-	edge2 := &iGraph.Edge {
-		Weight: 5,
-		Origin: state1,
-		Destination: goal,
-		IsDirected: true,
-	}
-	
-	openList := []*myNode{}
-	openList = append(openList, goal)
-	
-	edges := []*iGraph.Edge {edge1, edge2}
-	// graph := iGraph.Graph {
-	// 	Nodes: states,
-	// 	Edges: edges,
 	// }
 
-	pizi := piziStar{
-		start: start,
-		goal:  goal,
-		openList: openList,
-		states: []*myNode{state1, state2, state3},
-	}
+	// state1 := &myNode{
+	// 	tag: "new",
+	// }
+	// state2 := &myNode{
+	// 	tag: "new",
+	// }
+	// state3 := &myNode{
+	// 	tag: "new",
+	// }
+	// states := []iGraph.CustomNode {state1, state2, state3}
 
-	pizi.process_state()
+	// edge1 := &iGraph.Edge {
+	// 	Weight: 5,
+	// 	Origin: start,
+	// 	Destination: state1,
+	// 	IsDirected: true,
+	// }
+	// edge2 := &iGraph.Edge {
+	// 	Weight: 5,
+	// 	Origin: state1,
+	// 	Destination: goal,
+	// 	IsDirected: true,
+	// }
+	
+	// openList := []*myNode{}
+	// openList = append(openList, goal)
+	
+	// edges := []*iGraph.Edge {edge1, edge2}
+	// // graph := iGraph.Graph {
+	// // 	Nodes: states,
+	// // 	Edges: edges,
+	// // }
 
+	// pizi := piziStar{
+	// 	start: start,
+	// 	goal:  goal,
+	// 	openList: openList,
+	// 	states: []*myNode{state1, state2, state3},
+	// }
+
+	// pizi.process_state()
+		
+
+
+	testLand()
+}
+
+func testLand() {
+	land := Land{}
+	land.init(2, 3)
+	land.print()
 }
